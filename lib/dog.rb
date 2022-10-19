eeb9ab66780a92a5b26c45aad9b44f1c8571f6ea
nelsonmutindamwanzia <nmutinda02@gmail.com>
2022-10-19 15:54:53 +0300

    Complete Assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,38 @@
 class Dog
+    def name=(dog_name)
+        #this_dogs_name is a local variable. It therefore has local scope
+        #adding an "@" symbol before it makes it an instance variable.
+        @this_dogs_name = dog_name
+    end
 
+    def name
+        @this_dogs_name
+    end
 end
+
+lassie = Dog.new
+lassie.name = "Lassie"
+
+puts lassie.name
+
+
+
+# class Dog
+#     # setter method
+#     def name=(value)
+#         @name = value
+#     end
+
+#     # getter method
+#     def name
+#         # access the data from the @name instance variable and return it
+#         @name
+#     end
+# end
+
+# odie = Dog.new
+# odie.name = "Odie"
+
+# # call the Dog#name method
+# puts odie.name
+# # "Odie"
